e1735d23f0c3b99940f2b99d29b45604dfc0a01e
saiamith2000 <saiamith2000@gmail.com>
2024-01-21 06:51:08 +0530

    Added proxy

--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
-const express=require('express');
-const {ServerConfig,Logger}= require('./config');
-const apiRoutes=require('./routes');
-const { rateLimit } = require('express-rate-limit');
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const { ServerConfig, Logger } = require("./config");
+const apiRoutes = require("./routes");
+const { rateLimit } = require("express-rate-limit");
 
-const app=express();
-const limiter=rateLimit({
-windowMs:2*60*1000,
-max:4
+const app = express();
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000,
+  max: 4,
 });
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(limiter);
-app.use('/api',apiRoutes);
+app.use(
+  "/flightsService",
+  createProxyMiddleware({
+    target: ServerConfig.FLIGHT_SERVICE,
+    changeOrigin: true,
+    pathRewrite: { "^/flightsService": "/" },
+  })
+);
+app.use(
+  "/bookingService",
+  createProxyMiddleware({
+    target: ServerConfig.BOOKING_SERVICE,
+    changeOrigin: true,
+    pathRewrite: { "^/bookingService": "/" },
+  })
+);
+app.use("/api", apiRoutes);
 
-app.listen(ServerConfig.PORT,()=>{
-    console.log(`Successfully started server on port:${ServerConfig.PORT}`);
+app.listen(ServerConfig.PORT, () => {
+  console.log(`Successfully started server on port:${ServerConfig.PORT}`);
 });
